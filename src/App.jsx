f4cde12f591a9161d5a8d3b3275f5f7bd7a7002d
Akani Maluleke <akani@Akanis-Air.lan>
2025-07-24 15:04:20 +0200

    Add route for /campaigns and link to CampaignsPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import Studio from "./pages/Studio";
 import CMOModePage from "./pages/CMOModePage";
+import CampaignsPage from './pages/CampaignsPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<DashboardPage />} />
         <Route path="/studio" element={<Studio />} />
         <Route path="/cmo" element={<CMOModePage />} />
+        <Route path="/campaigns" element={<CampaignsPage />} />
       </Routes>
     </Router>
   );
