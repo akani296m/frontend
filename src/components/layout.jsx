a0e53ac0f8d1949fecde3bdbc36e7fd926365ad4
Akani Maluleke <akani@Akanis-Air.lan>
2025-07-24 16:32:17 +0200

    Fix layout: remove undefined SidebarProvider from layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,21 +1,10 @@
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/AppSidebar";
+import Sidebar from './Sidebar';
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout = ({ children }: LayoutProps) => {
+export default function Layout({ children }) {
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        <AppSidebar />
-        <SidebarInset className="flex-1">
-          {children}
-        </SidebarInset>
-      </div>
-    </SidebarProvider>
+    <div className="flex min-h-screen w-full">
+      <Sidebar />
+      <main className="flex-1 bg-white">{children}</main>
+    </div>
   );
-};
-
-export default Layout;
+}
